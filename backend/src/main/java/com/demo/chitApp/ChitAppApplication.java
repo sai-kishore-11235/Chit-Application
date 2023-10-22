bffe3d94abf22652303b5bb9142fedec4c013c5a
sai-kishore-11235 <sai.kishore.11235@gmail.com>
2023-10-22 19:23:57 +0530

    changes to initial repo

---
+++ backend/src/main/java/com/demo/chitApp/ChitAppApplication.java
@@ -0,0 +1,13 @@
+package com.demo.chitApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChitAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChitAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/demo/chitApp/ChitAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.chitApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChitAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
